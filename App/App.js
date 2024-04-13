916c07ba47173db2845bca42bc692dd8c20b0019
Abdul Rehman <abdulrehmang571@gmail.com>
2024-04-13 19:08:07 +0500

    created navbar for assignment-4

--- App/App.js
+++ App/App.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 
-const Para = () => (
-    <div>
-        <h2>Heading</h2>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-            voluptatum quibusdam est. Saepe et consectetur atque neque vel
-            mollitia nulla obcaecati temporibus, placeat facilis ex magnam ut
-            quidem rem tempora?
-        </p>
-    </div>
-);
-
-const Container = () => (
-    <div id="container">
-        <h1 className="title" key="h1">
-            h1 heading
-        </h1>
-        <h2 className="title" key="h2">
-            h2 heading
-        </h2>
-        <h3 className="title" key="h3">
-            h3 heading
-        </h3>
-
-        <Para />
-    </div>
-);
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <div className="logo-container icon-container">
+                    <img
+                        className="icon logo-img"
+                        src={new URL('logo.png', import.meta.url)}
+                        width="50px"
+                        height="50px"
+                    ></img>
+                    <h1>CoolWeb</h1>
+                </div>
+                <div className="search-bar-container">
+                    <input
+                        className="search-bar"
+                        placeholder="Search Here..."
+                    ></input>
+                    <img
+                        className="icon logo-img"
+                        src={new URL('search-icon.png', import.meta.url)}
+                        width="25px"
+                        height="25px"
+                    ></img>
+                </div>
+                <div className="profile-container icon-container">
+                    <img
+                        className="icon profile-img"
+                        src={new URL('profile.png', import.meta.url)}
+                        width="50px"
+                        height="50px"
+                    ></img>
+                    <p>@cooluser</p>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<Container />);
+root.render(<Header />);
