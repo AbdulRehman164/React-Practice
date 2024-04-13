13fc61d72e5acfe47f3138aa26b9ae7675095a0f
Abdul Rehman <abdulrehmang571@gmail.com>
2024-04-13 17:55:40 +0500

    Create the same element using JSX

--- App/App.js
+++ App/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 
-const h1 = React.createElement(
-    'h1',
-    { className: 'title', key: 'h1' },
-    'h1 heading'
+const container = (
+    <div id="container">
+        <h1 className="title" key="h1">
+            h1 heading
+        </h1>
+        <h2 className="title" key="h2">
+            h2 heading
+        </h2>
+        <h3 className="title" key="h3">
+            h3 heading
+        </h3>
+    </div>
 );
-const h2 = React.createElement(
-    'h2',
-    { className: 'title', key: 'h2' },
-    'h2 heading'
-);
-const h3 = React.createElement(
-    'h3',
-    { className: 'title', key: 'h3' },
-    'h3 heading'
-);
-
-const container = React.createElement('div', { id: 'container' }, [h1, h2, h3]);
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(container);
